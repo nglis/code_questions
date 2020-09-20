5dce6576ad9df55b8337d00acfe944990a7ca44f
menmai <nglisic10@gmail.com>
2020-09-20 15:13:47 -0600

    Towers of Hanoi Recursive Solution

---
+++ src/StacksAndQueues/TowersOfHanoi.java
@@ -0,0 +1,14 @@
+package StacksAndQueues;
+
+public class TowersOfHanoi {
+
+    public void moveDisks(int n, Tower origin, Tower destination, Tower buffer) {
+        if (n <= 0)
+            return;
+
+        moveDisks(n-1, origin, buffer, destination);
+        moveTop(origin, destination);
+        moveDisk(n-1, buffer, destination, origin);
+    }
+
+}
