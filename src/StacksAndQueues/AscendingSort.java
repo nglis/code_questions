9e7978702df95dac24f3930fe3b35cfbe56070e3
menmai <nglisic10@gmail.com>
2020-09-21 21:39:49 -0600

    Sort Stack in ascending order

--- src/StacksAndQueues/AscendingSort.java
+++ src/StacksAndQueues/AscendingSort.java
@@ -3,6 +3,19 @@ package StacksAndQueues;
 public class AscendingSort {
     // Using two stack only
 
+    public Stack solution(Stack toSort) {
+        Stack sorted = new Stack();
+        while (!toSort.isEmpty()) {
+            int tmp = toSort.pop();
+            while (!sorted.isEmpty() && tmp < sorted.peek())
+                toSort.push(sorted.pop());
+
+            sorted.push(tmp);
+        }
+
+        return sorted;
+    }
+
     public void sortStack(Stack toSort) {
 
         if (toSort.isEmpty())
