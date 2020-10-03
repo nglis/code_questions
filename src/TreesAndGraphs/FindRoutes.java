9dee5f801ba7a9d75ea92d40a49c6323f3e1a9d3
menmai <nglisic10@gmail.com>
2020-10-02 23:12:30 -0600

    Finds if there is a root between two nodes (Graph)

--- src/TreesAndGraphs/FindRoutes.java
+++ src/TreesAndGraphs/FindRoutes.java
@@ -1,4 +1,24 @@
 package TreesAndGraphs;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class FindRoutes {
+
+    // Basic search for if two vertices have an edge
+    public boolean hasEdge(Graph graph, Vertex a, Vertex b) {
+        ArrayList<Vertex> forA = graph.get(a);
+        ArrayList<Vertex> forB = graph.get(b);
+
+        for (Vertex v : forA) {
+            if (v.equals(b))
+                return true;
+        }
+
+        for (Vertex v : forB) {
+            if (v.equals(a))
+                return true;
+        }
+    }
+
 }
