56f581f89ee71ca1b9ade5a8e9ac52fd5fbf238e
menmai <nglisic10@gmail.com>
2020-10-06 20:10:50 -0600

    Create a BST with minimal height using an Array

--- src/TreesAndGraphs/ArrayToBST.java
+++ src/TreesAndGraphs/ArrayToBST.java
@@ -1,8 +1,26 @@
 package TreesAndGraphs;
 
+import javax.swing.tree.TreeNode;
+
 public class ArrayToBST {
 
     // Sorted (increasing) array with unique integer elements
     // Convert to a binary search tree with minimal height
+
+    TreeNode createMinimalBST(int arr[], int start, int end) {
+        if (end < start) {
+            return null;
+        }
+
+        int mid = (start + end) / 2;
+        TreeNode n = new TreeNode(arr[mid]);
+        n.left = createMinimalBST(arr, start, mid - 1);
+        n.right = createMinimalBST(arr, mid, end);
+        return n;
+    }
+
+    TreeNode createMinimalBST(int arr[]) {
+        createMinimalBST(arr, 0, arr.length -1);
+    }
 
 }
