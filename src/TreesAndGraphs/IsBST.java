5abd743ec6c3eb6b3564cbc73ec76180fc400482
menmai <nglisic10@gmail.com>
2020-10-15 22:53:35 -0600

    Check if Binary Tree is BST

--- src/TreesAndGraphs/IsBST.java
+++ src/TreesAndGraphs/IsBST.java
@@ -1,4 +1,36 @@
 package TreesAndGraphs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
 public class IsBST {
+    public boolean subTreeIsBST(Node root) {
+        if (root.left != null && root.left.data > root.data)
+            return false;
+
+        if (root.right != null && root.right.data < root.data)
+            return false;
+
+        return true;
+    }
+
+    public boolean isBST(Node root) {
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+
+        while (q.size() > 0) {
+            Node temp = q.remove();
+
+            if (!subTreeIsBST(temp))
+                return false;
+
+            if (temp.left != null)
+                q.add(temp.left);
+
+            if (temp.right != null)
+                q.add(temp.right);
+        }
+    }
 }
