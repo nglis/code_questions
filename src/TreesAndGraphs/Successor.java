0dae9f882a4a2af54760ea770f080d91f84c8e7c
menmai <nglisic10@gmail.com>
2020-10-22 20:25:38 -0600

    Find next in-order node in BST given a node

--- src/TreesAndGraphs/Successor.java
+++ src/TreesAndGraphs/Successor.java
@@ -1,6 +1,39 @@
 package TreesAndGraphs;
 
+import javax.swing.tree.TreeNode;
+
 public class Successor {
+
+    // Solution
+    public TreeNode inOrderSucc(TreeNode n) {
+        if (n == null)
+            return null;
+
+        if (n.right != null) {
+            return n. right;
+        } else {
+            TreeNode q = n;
+            TreeNode x = q.parent;
+            while (x != null && x.left != q) {
+                q = x;
+                x = q.parent;
+            }
+
+            return x;
+        }
+    }
+
+    // Solution helper
+
+    public TreeNode leftMostChild(TreeNode n) {
+        if (n == null)
+            return null;
+
+        while (n.left != null)
+            n = n.left;
+
+        return n;
+    }
 
     // Helper node to find first right node ancestor
     public Node helper (Node n) {
