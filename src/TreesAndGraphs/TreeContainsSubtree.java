1ffe4605a3cbd021623740e05e2b524990bca4aa
menmai <nglisic10@gmail.com>
2020-10-27 15:29:14 -0600

    Check if subtree exists within a tree

--- src/TreesAndGraphs/TreeContainsSubtree.java
+++ src/TreesAndGraphs/TreeContainsSubtree.java
@@ -1,6 +1,33 @@
 package TreesAndGraphs;
 
+import com.sun.source.tree.Tree;
+
 public class TreeContainsSubtree {
+
+    // Attempt
+    boolean TreesAreEqual(Node r1, Node r2) {
+        if (r1 == null && r2 == null)
+            return true;
+
+        if (r1 == null || r2 == null)
+            return false;
+
+        if (r1.data != r2.data)
+            return false;
+
+        return TreesAreEqual(r1.left, r2.left) && TreesAreEqual(r1.right, r2.right);
+    }
+
+    boolean existsEqualSubtree(Node r1, Node r2) {
+        if (r1 == null)
+            return false;
+
+        if (r1.data == r2.data)
+            if (TreesAreEqual(r1, r2))
+                return true;
+
+        return existsEqualSubtree(r1.left, r2) || existsEqualSubtree(r1.right, r2);
+    }
 
 
 }
