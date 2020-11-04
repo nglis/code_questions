9a507017ece5dff2725e3af273f82688c828184c
menmai <nglisic10@gmail.com>
2020-11-03 20:58:38 -0700

    Insert a set of bits into another set of bits

--- src/BitManipulation/InsertBit.java
+++ src/BitManipulation/InsertBit.java
@@ -2,6 +2,21 @@ package BitManipulation;
 
 public class InsertBit {
 
+    public int soln(int n, int m, int i, int j) {
+        int ones = ~0;
+
+        int leftMask = ones << (j + 1);
+        int rightMask = ((1 << i) - 1);
+
+        int mask = leftMask | rightMask;
+
+        int n_masked = n & mask;
+        int m_shifted = m << i;
+
+        return n_masked | m_shifted;
+
+    }
+
     // Insert a 32-bit number into another 32-bit number between given positions
     // i.e. A = 1000100, B = 101, i = 5, Result = 1101100
 
