dbd0f82d196d828e8da038eb3d3b98dae2bddda8
menmai <nglisic10@gmail.com>
2020-11-07 14:08:50 -0700

    Get next largest and next smallest ints

--- src/BitManipulation/SmallerLargerBits.java
+++ src/BitManipulation/SmallerLargerBits.java
@@ -2,21 +2,25 @@ package BitManipulation;
 
 public class SmallerLargerBits {
 
-    // Bit manipulatuin approach (get next) solution
+    // Bit manipulation approach (get previous) solution
 
-    public int getNext(int n) {
+    public int getPrevious(int n) {
+        // To get previous number, find rightmost 10, swap the bits, shift all following 1's left as possible to the 10
+
         int c = n;
         int c0 = 0;
         int c1 = 0;
 
-        while (((c & 1) == 0) && (c != 0)) {
-            c0++;
+        // Counts 1's before the rightmost 0 and shift c so that the last bit is 0
+        while ((c & 1) == 1) {
+            c1++;
             c >>= 1;
         }
 
-        while ((c & 1) == 1) {
-            c1++;
-            c  >>= 1;
+        // Count 0's before the rightmost 1 bit in the new c value
+        while ((c & 1) == 0) {
+            c0++;
+            c >>= 1;
         }
 
         if (c0 + c1 == 31 || c0 + c1 == 0) {
@@ -25,8 +29,55 @@ public class SmallerLargerBits {
 
         int p = c0 + c1;
 
+        // Set all bits after and including first 10 to 0's
+        n &= ((~0) << p + 1);
+
+        int mask = (1 << (c1 + 1)) - 1;
+
+        n |= mask << (c0 - 1);
+
+        // Set 0 where the first 10 was to 1
+        //n |= (1 << p);
+
+        n |= ~(1 << (c1 + 1)) << c0 - 1;
+        return n;
+    }
+
+    // Bit manipulation approach (get next) solution
+
+    public int getNext(int n) {
+        // Find rightmost occurrence of 01, swap bits and shift all following 1's as far right as possible
+        int c = n;
+        int c0 = 0;
+        int c1 = 0;
+
+        //  Find first 0 and shift c until 1 is first (c0 is number of 0's after rightmost 1)
+        while (((c & 1) == 0) && (c != 0)) {
+            c0++;
+            c >>= 1;
+        }
+
+        // Find rightmost 01 in whats left of c (where c1 is number of 1's after 0 in rightmost 01)
+        while ((c & 1) == 1) {
+            c1++;
+            c  >>= 1;
+        }
+
+        // If c's bit order is 111...111 0000....0000, c is all 1's or c = 0 return -1
+        if (c0 + c1 == 31 || c0 + c1 == 0) {
+            return -1;
+        }
+
+        // p is the first bit after the rightmost 01 combination
+        int p = c0 + c1;
+
+        // Change the 0 in 01 to a 1
         n |= (1 << p);
+
+        // Change the 1 in 01 to a 0
         n &= ~((1 << p) - 1);
+
+        // Shift trailing 1's to end of the byte
         n |= (1 << (c1 - 1)) - 1;
         return n;
     }
