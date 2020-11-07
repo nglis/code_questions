daae4ec17a8a8af41a0ee3466ffec33d24ee90a1
menmai <nglisic10@gmail.com>
2020-11-06 23:08:57 -0700

    Get next largest and next smallest ints

--- src/BitManipulation/SmallerLargerBits.java
+++ src/BitManipulation/SmallerLargerBits.java
@@ -2,6 +2,36 @@ package BitManipulation;
 
 public class SmallerLargerBits {
 
+    // Bit manipulatuin approach (get next) solution
+
+    public int getNext(int n) {
+        int c = n;
+        int c0 = 0;
+        int c1 = 0;
+
+        while (((c & 1) == 0) && (c != 0)) {
+            c0++;
+            c >>= 1;
+        }
+
+        while ((c & 1) == 1) {
+            c1++;
+            c  >>= 1;
+        }
+
+        if (c0 + c1 == 31 || c0 + c1 == 0) {
+            return -1;
+        }
+
+        int p = c0 + c1;
+
+        n |= (1 << p);
+        n &= ~((1 << p) - 1);
+        n |= (1 << (c1 - 1)) - 1;
+        return n;
+    }
+
+
     public void printSmallestAndLargest(int n) {
         boolean oneFound = false, zeroFound = false;
         int count = -1;
