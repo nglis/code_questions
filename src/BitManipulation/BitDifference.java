ad608b2eb2f543497f11b5b69e0940826abfc345
menmai <nglisic10@gmail.com>
2020-11-08 11:41:00 -0700

    Get bit difference between two ints

--- src/BitManipulation/BitDifference.java
+++ src/BitManipulation/BitDifference.java
@@ -2,8 +2,28 @@ package BitManipulation;
 
 public class BitDifference {
 
+    // Solution
+    public int bitDifferenceSol(int a, int b) {
+        int count = 0;
+
+        for (int c = a ^ b; c != 0; c = c >> 1)
+            count++;
+
+        return count;
+    }
+
+    // Better solution
+    public int bitDifferenceSol2(int a, int b) {
+        int count = 0;
+
+        for (int c = a ^ b; c != 0; c = c & (c - 1))
+            count++;
+
+        return count;
+    }
+
     // Attempt assuming unsigned integer
-    public int bitDifference (int a, int b) {
+    public int bitDifference(int a, int b) {
         int count = 0, i = 31;
 
         while (i > 0) {
