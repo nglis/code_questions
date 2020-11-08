18766297779236c7b8b83fd9be29f09a5c6005f6
menmai <nglisic10@gmail.com>
2020-11-08 12:46:20 -0700

    Swap odd and even bits in 32 bit int

---
+++ src/BitManipulation/SwapOddEvenBits.java
@@ -0,0 +1,28 @@
+package BitManipulation;
+
+public class SwapOddEvenBits {
+
+    public int solution (int n) {
+        return (((n & (0xaaaaaaaa)) >> 1) | ((n & (0x55555555)) << 1));
+    }
+
+    // Attempt
+    public int swapOddEven (int n) {
+        int temp = n;
+
+        while (temp != 0) {
+            int even = temp & 1;
+            int odd = (temp >> 1) & 1;
+
+            if (even > odd)
+                n = n & (~(0) - 1);
+
+            if (odd > even)
+                n = n & (~(0) - 2);
+
+            temp = temp >> 2;
+        }
+
+        return n;
+    }
+}
