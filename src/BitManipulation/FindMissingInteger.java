1b3c51fe2f9f40d36fb741cd404fd5f771f0a9db
menmai <nglisic10@gmail.com>
2020-11-11 21:25:21 -0700

    Find missing integer in an array

--- src/BitManipulation/FindMissingInteger.java
+++ src/BitManipulation/FindMissingInteger.java
@@ -3,4 +3,16 @@ package BitManipulation;
 public class FindMissingInteger {
 
     // Find the missing integer in an array
+    public int missingInt(int[] values) {
+        int lastBit = -1;
+
+        for (int i = 0; i < values.length; i++) {
+            if (lastBit == (values[i] & 1))
+                return values[i] - 1;
+
+            lastBit = (values[i] & 1);
+        }
+
+        return -1;
+    }
 }
