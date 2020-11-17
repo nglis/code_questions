ff0f97bca585c3cd941f51e7bb58bbf17add9385
menmai <nglisic10@gmail.com>
2020-11-16 19:09:17 -0700

    Find line that splits two squares in half

---
+++ src/MathAndProbability/LineThroughSquares.java
@@ -0,0 +1,15 @@
+package MathAndProbability;
+
+public class LineThroughSquares {
+
+    class Line {
+        int slope;
+        int yIntercept;
+
+        public Line(int s, int y) {
+            slope = s;
+            yIntercept = y;
+        }
+    }
+
+}
