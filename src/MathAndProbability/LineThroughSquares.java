f4a37e227f67db645b7ccf73b6ae175c722153b7
menmai <nglisic10@gmail.com>
2020-11-16 19:12:49 -0700

    Find line that splits two squares in half

--- src/MathAndProbability/LineThroughSquares.java
+++ src/MathAndProbability/LineThroughSquares.java
@@ -12,4 +12,18 @@ public class LineThroughSquares {
         }
     }
 
+    public Line infiniteLine(Square A, Square B) {
+        // Assuming Square object has centerpoint (x,y)
+        int x_1 = A.center_x;
+        int y_1 = A.center_y;
+
+        int x_2 = B.center_x;
+        int y_2 = B.center_y;
+
+        int m = (y_2 - y_1) / (x_2 - x_1);
+        int intercept = y_1 - m * x_1;
+
+        return new Line(m, intercept);
+    }
+
 }
