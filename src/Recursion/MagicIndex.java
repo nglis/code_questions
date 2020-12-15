8782dd6f339c2446b2e801745404f45f187dfc73
menmai <nglisic10@gmail.com>
2020-12-14 21:29:00 -0700

    Find magic index

--- src/Recursion/MagicIndex.java
+++ src/Recursion/MagicIndex.java
@@ -12,6 +12,7 @@ public class MagicIndex {
         return -1;
     }
 
+    // Binary search for magic index (distinct, sorted array values)
     public int solution(int[] arr, int start, int end) {
         if (end < start || start < 0 || end >= arr.length) {
             return -1;
@@ -30,6 +31,29 @@ public class MagicIndex {
 
     public int solutionHelper(int[] arr) {
         return solution(arr, 0, arr.length - 1);
+    }
+
+    // Binary like search for magic index (non-distinct, sorted array values)
+    public int solution2(int[] arr, int start, int end) {
+        if (end < start || start < 0 || end >= arr.length) {
+            return -1;
+        }
+
+        int mid = (start + end) / 2;
+
+        if (arr[mid] == mid) {
+            return mid;
+        }
+
+        int leftIndex = Math.min(mid - 1, arr[mid]);
+        int left = solution2(arr, start, leftIndex);
+        if (left >= 0) {
+            return left;
+        }
+
+        int rightIndex = Math.max(mid + 1, arr[mid]);
+        int right = solution2(arr, rightIndex, end);
+        return right;
     }
 
 }
