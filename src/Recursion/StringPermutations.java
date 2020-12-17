e66096eafa62bb7c77c1d12a5b00caf0845e1fa6
menmai <nglisic10@gmail.com>
2020-12-16 23:59:16 -0700

    Find all permutations of a string

---
+++ src/Recursion/StringPermutations.java
@@ -0,0 +1,37 @@
+package Recursion;
+
+import java.util.ArrayList;
+
+public class StringPermutations {
+
+    public ArrayList<String> getPermutations(String str) {
+        if (str == null) {
+            return null;
+        }
+
+        ArrayList<String> permutations = new ArrayList<String>();
+        if (str.length() == 0) {
+            permutations.add("");
+            return permutations;
+        }
+
+        char first = str.charAt(0);
+        String remainder = str.substring(1);
+        ArrayList<String> words = getPermutations(remainder);
+        for (String word : words) {
+            for (int j = 0; j <= word.length(); j++) {
+                String s = insertCharAt(word, first, j);
+                permutations.add(s);
+            }
+        }
+
+        return permutations;
+    }
+
+    public static String insertCharAt(String word, char c, int i) {
+        String start = word.substring(0, i);
+        String end = word.substring(i);
+        return start + c + end;
+    }
+
+}
