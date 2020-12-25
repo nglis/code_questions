4613031d3e8f43249474c7cfc2c5be436904364a
menmai <nglisic10@gmail.com>
2020-12-25 00:13:20 -0700

    Sort string array by anagrams

--- src/Recursion/AnagramSort.java
+++ src/Recursion/AnagramSort.java
@@ -1,9 +1,62 @@
 package Recursion;
 
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.LinkedList;
+
 public class AnagramSort {
 
     // Sort an array of strings so anagrams are next to each other
-    // Assume a stable sort
+
+    void sort(String[] arr) {
+        Hashtable<String, LinkedList<String>> hash = new Hashtable<>();
+
+        for (String s : arr) {
+            String key = sortChars(s);
+            if (!hash.containsKey(key)) {
+                hash.put(key, new LinkedList<String>());
+            }
+
+            LinkedList<String> anagrams = hash.get(key);
+            anagrams.push(s);
+        }
+
+        int index = 0;
+        for (String key : hash.keySet()) {
+            LinkedList<String> list = hash.get(key);
+            for (String t :list) {
+                arr[index] = t;
+                index++;
+            }
+        }
+
+    }
+
+    String sortChars(String s) {
+        char[] content = s.toCharArray();
+        Arrays.sort(content);
+        return new String(content);
+    }
+
+    int compare(String s1, String s2) {
+        return sortChars(s1).compareTo(sortChars(s2));
+    }
+
+    void anagramSort(String[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (isAnagram(arr[i], arr[i + 1]))
+                continue;
+
+            for (int j = i + 2; j < arr.length; i++) {
+                if (isAnagram(arr[i], arr[j])) {
+                    String temp = arr[j];
+                    arr[j] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    break;
+                }
+            }
+        }
+    }
 
     boolean isAnagram(String s1, String s2) {
         if (s1.length() != s2.length())
