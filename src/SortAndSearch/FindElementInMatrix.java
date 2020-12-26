cd2cac6a1965edf14e2d47a07be04eb5228acdb3
menmai <nglisic10@gmail.com>
2020-12-25 18:01:59 -0700

    Check if element exists in matrix

---
+++ src/SortAndSearch/FindElementInMatrix.java
@@ -0,0 +1,25 @@
+package SortAndSearch;
+
+public class FindElementInMatrix {
+
+    // Find element in a M x N matrix
+
+    public boolean search(int[][] matrix, int elem) {
+        int row = 0;
+        int col = matrix[0].length;
+
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == elem) {
+                return true;
+            }
+            if (elem < matrix[row][col]) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+
+        return false;
+    }
+
+}
